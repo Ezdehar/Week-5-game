cd8160257c4d03c3df292edad24c851d5981ac4f
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-02-24 15:12:20 -0500

    added files from week 4

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+var targetNumber = 39
+var selectedNumber = "";
+var numberOptions = [10, 5, 3, 7];
+var numBlank = 0;
+var blankAndSuccesses = []
+
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var totalScoreCount = ;
+
+
+function startGame () {
+   selectedNumber = numberOptions[Math.floor(Math.random() * numberOptions.length)];
+   numberOptions = selectedNumber.length
+   targerNumber = number-to-guess
+
+   console.log(targetNumber);
+   console.log(selectedNumber);
+   console.log(numBlank);
+}
+ 
+  for (var i = 0; i < numberOptions.length; i++) {
+      blankAndSuccesses.push("");
+}
+
+document.getElementById("numberToGuess"){
+
+  }
+
+
+<script type="text/javascript">
+
+  $(document).ready(function() {
+      
+  $("#.crystal-image-1").on("click", function(button) {
+   var button = $(this).attr("value");
+   console.log("this is button 1");
+   $("crystal-image").text(random);
+  });
+
+  $("#.crystal-image-2").on("click", function(button) {
+    var button = $(this).attr("value");
+    console.log("this is button 2");
+    $("#crystal-image").text(random);
+  });
+
+ $(".crystal-image-3").on("click", function(button) {
+    var button = $(this).attr("value");
+    console.log("this is button 3");
+    $("#.crystal-image").text(random);
+  });
+
+  $("#.crystal-image-4").on("click", function(button) {
+    var button = $(this).attr("value");
+    console.log("this is button 4");
+    $("#.crystal-image").text(random);
+  });
+
+
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
